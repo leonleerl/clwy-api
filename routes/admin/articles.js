5b7a91952644cc2db9a941f8ae91139da63836d3
leonleerl <leon.runlongli@gmail.com>
2024-08-25 21:18:55 +0800

    查询单个文章

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -24,4 +24,31 @@ router.get("/", async function (req, res) {
   }
 });
 
+router.get("/:id", async function (req, res) {
+  try {
+    const { id } = req.params;
+    const article = await Article.findByPk(id);
+    if (article) {
+      res.json({
+        status: true,
+        message: "查询文章成功",
+        data: {
+          article,
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "文章未找到",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "查询文章失败",
+      errors: [eroor.message],
+    });
+  }
+});
+
 module.exports = router;
