b161155fc890cb6b093eec8f92906c07421fc244
leonleerl <leon.runlongli@gmail.com>
2024-08-26 09:01:01 +0800

    创建和删除文章

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -51,4 +51,35 @@ router.get("/:id", async function (req, res) {
   }
 });
 
+router.post("/", async function (req, res) {
+  try {
+    const article = await Article.create(req.body);
+    res.status(201).json({
+      status: true,
+      message: "创建文章成功",
+      data: article,
+    });
+  } catch (error) {}
+});
+
+router.delete("/:id", async function (req, res) {
+  try {
+    const { id } = req.params;
+    const article = await Article.findByPk(id);
+    if (article) {
+      await article.destroy();
+      res.json({
+        status: true,
+        message: "删除文章成功",
+      });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "删除文章失败",
+        errors: [error.message],
+      });
+    }
+  } catch (error) {}
+});
+
 module.exports = router;
