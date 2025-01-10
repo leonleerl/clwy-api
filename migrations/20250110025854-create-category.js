93a965534a32341f3f2d4e2f87402e14b5425214
leonleerl <leon.runlongli@gmail.com>
2025-01-10 11:07:51 +0800

    add create-category migration

--- migrations/20250110025854-create-category.js
+++ migrations/20250110025854-create-category.js
@@ -1,31 +1,35 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Categories', {
+    await queryInterface.createTable("Categories", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER.UNSIGNED,
       },
       name: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       rank: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        defaultValue: 1,
+        type: Sequelize.INTEGER.UNSIGNED,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Categories');
-  }
+    await queryInterface.dropTable("Categories");
+  },
 };
